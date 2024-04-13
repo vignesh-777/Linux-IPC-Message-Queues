e6cf7df96c42a0113b9a90e387472bd14fea621a
vignesh-777 <r.vignesh53612@gmail.com>
2024-04-13 10:09:38 +0530

    commit

--- reader.c
+++ reader.c
@@ -12,13 +12,13 @@ int main()
 {
 	key_t key;
 	int msgid;
-// ftok to generate unique key
+    	// ftok to generate unique key
 	key = ftok("progfile", 65);
 	// msgget creates a message queue
 	// and returns identifier
-	msgid = 
+	msgid = msgget(key, 0666 | IPC_CREAT);
 	// msgrcv to receive message
-	
+	msgrcv(msgid, &message, sizeof(message), 1, 0);
 	// display the message
 	printf("Data Received is : %s \n",
 			message.mesg_text);
@@ -27,3 +27,4 @@ int main()
 	msgctl(msgid, IPC_RMID, NULL);
 	return 0;
 }
+
